bd3f6017d3dd6e777b89742d19f1ce08001fd590
victor <cooldan.tw@gmail.com>
2018-07-28 13:33:51 +0800

    web page

--- web/public/dashboard.js
+++ web/public/dashboard.js
@@ -1,3 +1,7 @@
+const page_count = 10;
+const max_page = 4;
+let current_page = 0;
+let drawTableByPage = null;
 function getJWT(){
   var url = window.location.href;
   let params = new URL(url).searchParams;
@@ -148,8 +152,7 @@ function drawLineGraph(data){
   //      return d.name ;
   //    });
 };
-
-function drawTable(data){
+function drawTableHead(data){
   var rows_data = translateDataForTable(data);
   let uniq_keys = [];
   R.forEach((obj) => {
@@ -162,46 +165,94 @@ function drawTable(data){
     R.prepend('updated_time'),
     R.reject(R.equals("updated_time"))
   )(uniq_keys);
-  console.log(uniq_keys);
-  let table = d3.select('#currencyList');
-  table.append('thead')
-       .append('tr')
-       .selectAll('th')
-       .data(uniq_keys)
-       .enter().append('th')
-       .text(function(d){
-         if(d == "updated_time") return "#";
-         return d;
+  let head = d3.select('#head_id');
+  head.exit().remove();
+  let head_col = head.selectAll('th')
+  let update_head = head_col.data(uniq_keys, d => {
+         return { text: d, date: new Date() };
        });
-   
-   let rows = table.append('tbody')
-                   .selectAll('tr')
-                   .data(rows_data)
-                   .enter()
-                   .append('tr');
-   let cells = rows.selectAll('td')
-                   .data((row) => {
-                     return uniq_keys.map(column => {
-                       return { column: column, value: row[column] };  
+  update_head.exit().remove();
+  let enter_head = update_head.enter()
+                              .append('th')
+  enter_head.text(function(d){
+    if(d == "updated_time") return "#";
+    return d;
+  });
+  return uniq_keys;
+}
+function drawTable(data, uniq_keys, page){
+  var rows_data = translateDataForTable(data);
+  const begin = page * page_count;
+  const end = begin + page_count;
+  rows_data = rows_data.slice(begin, end);
+  let table = d3.select('#currencyList');
+  let table_body = table.select('#tbody_id')
+  table_body.exit().remove();
+  let rows = table_body.selectAll('tr');
+  let updated_rows = rows.data(rows_data, d => {
+    return d.updated_time;
+  });
+  updated_rows.exit().remove();
+  let enter_rows = updated_rows.enter().append('tr');
+  let cells = enter_rows.selectAll('td')
+                  .data((row) => {
+                     let update_col_data = uniq_keys.map(column => {
+                       return { column: column, value: row[column], date: new Date()};  
                      });
-                   }).enter()
-                   .append('td')
-                   .text(d => {
-                     if(d.column == "updated_time"){
-                       let updated_time = new Date(d.value);
-                       let hours = updated_time.getHours();
-                       let amOpm = hours >= 12 ? 'pm' : 'am';
-                       hours = hours % 12;
-                       hours = hours ? hours : 12;
-                       let min = updated_time.getMinutes();
-                       min = min < 10 ? '0' + min : min;
-                       return updated_time.getFullYear() + "-" +
-                              (updated_time.getMonth() + 1) + "-" +
-                              updated_time.getDate() + " " +
-                              hours + ":" + min + " " + amOpm;
-                     }
-                     return d.value ? d.value : '-';
-                   });
+                     return update_col_data;
+                  });
+   cells.exit().remove();
+   cells.enter()
+        .append('td')
+        .text(d => {
+          if(d.column == "updated_time"){
+            let updated_time = new Date(d.value);
+            let hours = updated_time.getHours();
+            let amOpm = hours >= 12 ? 'pm' : 'am';
+            hours = hours % 12;
+            hours = hours ? hours : 12;
+            let min = updated_time.getMinutes();
+            min = min < 10 ? '0' + min : min;
+            return updated_time.getFullYear() + "-" +
+              (updated_time.getMonth() + 1) + "-" +
+              updated_time.getDate() + " " +
+              hours + ":" + min + " " + amOpm;
+            }
+            return d.value ? d.value : '-';
+        });
+  enter_rows.exit().remove();
+}
+function drawPageButton(data){
+  let li = d3.select('#page')
+             .selectAll('li')
+             .data(['prev', '1', '2', '3', 'next'])
+             .enter()
+             .append('li')
+             .classed('page-item', true)
+             .attr('id', d => {
+               if(d == 'prev') return 'page_' + 0;
+               if(d == 'next') return 'page_' + 4;
+               return 'page_' + d;
+             });
+
+  li.append('a')
+    .datum(d => d)
+    .classed('page-link', true)
+    .text(d => d)
+    .on('click', d => {
+      if (d == 'prev'){
+        current_page = current_page > 0 ? current_page - 1 : current_page;
+      } else if(d == 'next'){
+        current_page = current_page < max_page ? current_page + 1 : current_page;
+      } else {
+        current_page = parseInt(d);
+      }
+      drawTableByPage(current_page);
+      d3.select('#page').selectAll('li').classed('active', false);
+      // const id = d3.event.target.id;
+      d3.select('#page_' + current_page).classed('active', true);
+      // d3.select(d3.event.target.parentNode).classed('active', true);
+    });
 }
 function getCurrencyData(){
   d3.json(
@@ -210,7 +261,12 @@ function getCurrencyData(){
     drawLineGraph(data);
     return data;  
   }).then(data => {
-    drawTable(data);
+    const uniq_keys = drawTableHead(data)
+    drawTableByPage = R.partial(drawTable, [data, uniq_keys]);
+    drawTableByPage(0);
+    return data;
+  }).then(data => {
+    drawPageButton();
     return data;
   });
 };
